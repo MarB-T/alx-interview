41162e220d8b4c8a10a5bf03c9a612e8d675105c
MarB-T <danmarbet@gmail.com>
2024-01-10 14:39:53 +0300

    lockboxes answer1

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Find if all th 'boxes' can be unlocked
+"""
+
+def canUnlockAll(boxes):
+    """function to check if all boxes unlock"""
+    all_index = set(list(range(len(boxes))))
+    keys = {0}
+    keys.update(boxes[0])
+    for _ in range(2):
+        for i in range(len(boxes)):
+            if i in keys:
+                keys.update(boxes[i])
+    
+    return (keys == all_index)
