f3927757231dc1eb8aadfa53ae9a948203a8c5be
MarB-T <danmarbet@gmail.com>
2024-01-10 14:57:23 +0300

    lockboxes answer1 correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,14 +4,14 @@
 Find if all th 'boxes' can be unlocked
 """
 
+
 def canUnlockAll(boxes):
     """function to check if all boxes unlock"""
     all_index = set(list(range(len(boxes))))
     keys = {0}
     keys.update(boxes[0])
-    for _ in range(2):
+    for _ in range(3):
         for i in range(len(boxes)):
             if i in keys:
-                keys.update(boxes[i])
-    
+                keys.update(boxes[i]) 
     return (keys == all_index)
