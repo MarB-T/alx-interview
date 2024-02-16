ff27e2465437ea3d67257200ac0c92215bfb1165
MarB-T <danmarbet@gmail.com>
2024-02-16 04:32:25 +0300

    Starwars characters correction

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,21 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
+const argv = process.argv[2];
+const urlFilm = 'https://swapi-api.alx-tools.com/api/films/';
+const url = `${urlFilm}${argv}/`;
 
-const request = require('request');
+const req = require('request');
+const util = require('util');
+const request = util.promisify(req);
 
-request(urlMovie, function (error, response, body) {
-  if (error === null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
-
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
-    }
-  } else {
-    console.log(error);
+async function star (url) {
+  const body = await (await request(url)).body;
+  const pot = JSON.parse(body);
+  const people = pot.characters;
+  for (const character of people) {
+    const bod = await (await request(character)).body;
+    const person = JSON.parse(bod);
+    console.log(person.name);
   }
-});
-
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-  request(url, function (error, response, body) {
-    if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
-    } else {
-      console.error('error:', error);
-    }
-  });
 }
+
+star(url);
