4d40b574cf8226bba54d11b380674d1a35f0b5bd
MarB-T <danmarbet@gmail.com>
2024-02-16 04:38:45 +0300

    Starwars characters correction1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,20 @@
 #!/usr/bin/node
+// star wars api
+const reques = require('request');
+const util = require('util');
+const request = util.promisify(reques);
 
-const request = require('request');
-const arg = process.argv[2];
-
-async function retPro (url) {
-  return new Promise(function (resolve, reject) {
-    request(url, function (err, res, body) {
-      resolve(JSON.parse(body).name);
-      if (err) throw err;
-    });
-  });
-}
-
-async function chars () {
-  return new Promise(function (resolve, reject) {
-    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, function (err, res, bod) {
-      resolve(JSON.parse(bod).characters);
-      if (err) throw err;
-    });
-  });
-}
-
-async function names () {
-  const thischars = await chars();
-  for (let i = 0; i < thischars.length; i++) {
-    console.log(await retPro(thischars[i]));
+async function star (url) {
+  const body = await (await request(url)).body;
+  const pot = JSON.parse(body);
+  const people = pot.characters;
+  for (const character of people) {
+    const bod = await (await request(character)).body;
+    const person = JSON.parse(bod);
+    console.log(person.name);
   }
 }
 
-names();
+const value = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + value;
+star(url);
